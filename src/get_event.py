2506d18a7a8660e8aa174a422fcef1b45a4550f9
masaki12-s <asam.archer1112@yahoo.ne.jp>
2022-09-20 06:13:36 +0900

    途中

--- src/get_event.py
+++ src/get_event.py
@@ -1,18 +1,25 @@
+from inspect import getcallargs
 import os
 import caldav
 import datetime
+from dotenv import load_dotenv
 
+load_dotenv()
+url = os.getenv('url')
+username = os.getenv('user')
+password = os.getenv('password')
 
-
-def getevent(days=1):
-    url = os.getenv('url')
-    username = os.getenv('username')
-    password = os.getenv('password')
-    client = caldav.DAVClient(url=url ,username=username, password=password)
+def getCalendar(url=url, username=username, password=password):
+    client = caldav.DAVClient(url=url, username=username, password=password)
     principal = client.principal()
     calendars = principal.calendars()
+    return calendars
+
+def getevent(days=1):
     today = datetime.datetime.now()
     schedules = []
+    calendars = getCalendar()
+
     for calendar in calendars:
             try:
                 events = calendar.date_search(start=today, end=today+datetime.timedelta(days=days), expand=True)
@@ -24,19 +31,25 @@ def getevent(days=1):
                     schedules.append(d)
             except:
                 continue
+    return schedules
 
-    d = {}
+def a(schedules: list):
     for schedule in schedules:
-        if 'SUMMARY' in schedule.keys():
-            key = schedule['SUMMARY']
-        if 'DTSTART' in schedule.keys():
-            value1 = schedule['DTSTART'][:8]
-        elif 'DTSTART;VALUE=DATE' in schedule.keys():
-            value1 = schedule['DTSTART;VALUE=DATE'][:8]
+
+    # d = {}
+    # for schedule in schedules:
+    #     if 'SUMMARY' in schedule.keys():
+    #         key = schedule['SUMMARY']
+    #     if 'DTSTART' in schedule.keys():
+    #         value1 = schedule['DTSTART'][:8]
+    #     elif 'DTSTART;VALUE=DATE' in schedule.keys():
+    #         value1 = schedule['DTSTART;VALUE=DATE'][:8]
         
-        if 'DTEND' in schedule.keys():
-            value2 = schedule['DTSTART'][:8]
-        elif 'DTEND;VALUE=DATE' in schedule.keys():
-            value2 = schedule['DTEND;VALUE=DATE'][:8]
-        d[key] = (value1,value2)
-    print(d)
+    #     if 'DTEND' in schedule.keys():
+    #         value2 = schedule['DTSTART'][:8]
+    #     elif 'DTEND;VALUE=DATE' in schedule.keys():
+    #         value2 = schedule['DTEND;VALUE=DATE'][:8]
+    #     d[key] = (value1,value2)
+    # print(d)
+
+print(getevent())
